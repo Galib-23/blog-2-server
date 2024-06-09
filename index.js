21b9fe4d2d3abe4190f2f52dafa117294a04451f
Galib-23 <aagalib2323@gmail.com>
2024-06-09 21:02:15 +0600

    database connected

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+dotenv.config();
 
-app.get("/", (req, res)=> {
-    res.send("Blog server is running...")    
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB is connected!");
+  })
+  .catch((error) => {
+    console.log("Error in mongo connection: ", error);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Blog server is running...");
+});1024
 
 app.listen(PORT, () => {
-    console.log(`Blog server running on port ${PORT}`)
-})
+  console.log(`Blog server running on port ${PORT}`);
+});
