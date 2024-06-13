85a360e0ef1b94e718a099b0de10a828c65842e6
Galib-23 <aagalib2323@gmail.com>
2024-06-13 22:03:48 +0600

    Create google user authetication api route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,66 +1,113 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorThrower } from "../utils/error.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
-    try {
-        const { username, email, password } = req.body;
-    
-        if(!username || !email || !password || username === '' || email === '' || password === ''){
-            return next(errorThrower(400, 'All fields required'))
-        }
+  try {
+    const { username, email, password } = req.body;
 
-        const hashedPassword = bcryptjs.hashSync(password, 10);
-
-        const newUser = new User({
-            username,
-            email,
-            password: hashedPassword
-        });
-        const savedUser = await newUser.save();
-
-        const userResponse = savedUser.toObject();
-        delete userResponse.password;
-
-
-        return res.status(200).send(userResponse);
-    } catch (error) {
-        return next(error);
+    if (
+      !username ||
+      !email ||
+      !password ||
+      username === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      return next(errorThrower(400, "All fields required"));
     }
-}
 
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    const savedUser = await newUser.save();
+
+    const userResponse = savedUser.toObject();
+    delete userResponse.password;
+
+    return res.status(200).send(userResponse);
+  } catch (error) {
+    return next(error);
+  }
+};
 
 export const signin = async (req, res, next) => {
-    const { email, password } = req.body;
-    if(!email || !password || email === '' || password === ''){
-        return next(errorThrower(400, 'All fields required'))
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    return next(errorThrower(400, "All fields required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorThrower(404, "User not found"));
     }
-    try {
-        const validUser = await User.findOne({email});
-        if (!validUser) {
-            return next(errorThrower(404, "User not found"))
-        }
-        const validPassword = bcryptjs.compareSync(password, validUser.password);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
 
-        if (!validPassword) {
-            return next(errorThrower(400, 'Wrong Credentials'));
-        }
-
-        const token = jwt.sign(
-            {id: validUser._id},
-            process.env.JWT_SECRET,
-            {expiresIn: '10d'}
-        );
-
-        const { password: _, ...rest } = validUser._doc;
-
-        return res.status(200).cookie('access-token', token, {
-            httpOnly: true,
-        }).json(rest);
-
-
-    } catch (error) {
-        return next(error);
+    if (!validPassword) {
+      return next(errorThrower(400, "Wrong Credentials"));
     }
-}
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "10d",
+    });
+
+    const { password: _, ...rest } = validUser._doc;
+
+    return res
+      .status(200)
+      .cookie("access-token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
+  const { email, name, googlePhotoUrl } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password, ...rest } = user._doc;
+      return res
+        .status(200)
+        .cookie("access-token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    }
+    const generatedPassword =
+      Math.random().toString(36).slice(-8) +
+      Math.random().toString(36).slice(-8);
+
+    const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+    const newUser = new User({
+      username:
+        name.toLowerCase().split(" ").join("") +
+        Math.random().toString(9).slice(-4),
+      email,
+      password: hashedPassword,
+      profilePicture: googlePhotoUrl,
+    });
+    const userRes = await newUser.save();
+    const token = jwt.sign({ id: userRes._id }, process.env.JWT_SECRET);
+    const newUserObj = userRes.toObject();
+    delete newUserObj.password;
+
+    res
+      .status(200)
+      .cookie("access-token", token, {
+        httpOnly: true,
+      })
+      .json(newUserObj);
+  } catch (error) {
+    return next(error);
+  }
+};
