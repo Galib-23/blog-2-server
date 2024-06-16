d421edddeb4e03b7aac779c17b51dbfd8ca570d3
Galib-23 <aagalib2323@gmail.com>
2024-06-16 13:37:05 +0600

    Fix conditions of user update api route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,6 +7,25 @@ export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.id) {
         return next(errorThrower(401, 'You can only update your own account!'));
     }
+    if(req.body.password){
+        if(req.body.password.length < 6){
+            return next(errorThrower(401, 'Password must be at least 6 characters'));
+        }
+    }
+    if(req.body.username){
+        if(req.body.username.length < 6 || req.body.username.length > 20){
+            return next(errorThrower(401, 'Username must be between 6 to 20 characters'));
+        }
+    }
+    if(req.body.username.includes(' ')){
+        return next(errorThrower(401, 'Username can not contain spaces'));
+    }
+    if(req.body.username !== req.body.username.toLowerCase()){
+        return next(errorThrower(401, 'Username must be lowercase'));
+    }
+    if(!req.body.username.match(/^[a-zA-Z0-9]+$/)){
+        return next(errorThrower(401, 'username can only contain letters and numbers'));
+    }
     try {
         if(req.body.password){
             req.body.password = bcryptjs.hashSync(req.body.password, 10);
