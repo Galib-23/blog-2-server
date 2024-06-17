2c8da50a53b782d938b253029bb12fb14a591d97
Galib-23 <aagalib2323@gmail.com>
2024-06-17 22:34:53 +0600

    fix update user conditions

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,49 +1,55 @@
-import { errorThrower } from "../utils/error.js"
+import { errorThrower } from "../utils/error.js";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-
 export const updateUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id) {
-        return next(errorThrower(401, 'You can only update your own account!'));
+  if (req.user.id !== req.params.id) {
+    return next(errorThrower(401, "You can only update your own account!"));
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(errorThrower(401, "Password must be at least 6 characters"));
     }
-    if(req.body.password){
-        if(req.body.password.length < 6){
-            return next(errorThrower(401, 'Password must be at least 6 characters'));
-        }
+  }
+  if (req.body.username) {
+    if (req.body.username.length < 6 || req.body.username.length > 20) {
+      return next(
+        errorThrower(401, "Username must be between 6 to 20 characters"),
+      );
     }
-    if(req.body.username){
-        if(req.body.username.length < 6 || req.body.username.length > 20){
-            return next(errorThrower(401, 'Username must be between 6 to 20 characters'));
-        }
+    if (req.body.username.includes(" ")) {
+      return next(errorThrower(401, "Username can not contain spaces"));
     }
-    if(req.body.username.includes(' ')){
-        return next(errorThrower(401, 'Username can not contain spaces'));
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorThrower(401, "Username must be lowercase"));
     }
-    if(req.body.username !== req.body.username.toLowerCase()){
-        return next(errorThrower(401, 'Username must be lowercase'));
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorThrower(401, "username can only contain letters and numbers"),
+      );
     }
-    if(!req.body.username.match(/^[a-zA-Z0-9]+$/)){
-        return next(errorThrower(401, 'username can only contain letters and numbers'));
+  }
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
-    try {
-        if(req.body.password){
-            req.body.password = bcryptjs.hashSync(req.body.password, 10);
-        }
 
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-            $set: {
-                username: req.body.username,
-                password: req.body.password,
-                profilePicture: req.body.profilePicture
-            }
-        }, {new: true});
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
+        },
+      },
+      { new: true },
+    );
 
-        const { password, ...rest} = updatedUser._doc;
+    const { password, ...rest } = updatedUser._doc;
 
-        res.status(200).json(rest);
-
-    } catch (error) {
-        next(error);
-    }
-}
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
