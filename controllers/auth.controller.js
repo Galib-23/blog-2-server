0483c57d89996a269b09cf955171bd5cdc7c2781
Galib-23 <aagalib2323@gmail.com>
2024-06-19 22:06:53 +0600

    add delete user api route and controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -74,7 +74,7 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {expiresIn: '10d'});
       const { password, ...rest } = user._doc;
       return res
         .status(200)
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -53,3 +53,15 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorThrower(403, "Unauthorized"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
