54bf563791efdf49ec19ddc45a5a641b833086fe
Galib-23 <aagalib2323@gmail.com>
2024-07-10 14:27:24 +0600

    Add likecomment and delete comment api route

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -30,3 +30,40 @@ export const getPostComments = async (req, res, next) => {
         next(error);
     }
 }
+
+export const likeComment = async (req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        if (!comment) {
+            return next(errorThrower(404, "comment not found"))
+        }
+        const userIndex = comment.likes.indexOf(req.user.id);
+        if (userIndex === -1) {
+            comment.numberOfLikes += 1;
+            comment.likes.push(req.user.id);
+        }else{
+            comment.numberOfLikes -= 1;
+            comment.likes.splice(userIndex, 1);
+        }
+        await comment.save();
+        res.status(200).json(comment);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const deleteComment = async (req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params.comment);
+        if (!comment) {
+            return next(errorThrower(404, "comment not found"))
+        }
+        if (!req.user.isAdmin) {
+            return next(errorThrower(403, "You are not allowed to delete this comment"))
+        }
+        await Comment.findByIdAndDelete(req.params.commentId);
+        res.status(200).json("comment has been deleted");
+    } catch (error) {
+        next(error);
+    }
+}
--- routes/comment.route.js
+++ routes/comment.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createComment, getPostComments } from '../controllers/comment.controller.js';
+import { createComment, deleteComment, getPostComments, likeComment } from '../controllers/comment.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment)
-router.post('/getPostComments/:postId', getPostComments)
+router.get('/getPostComments/:postId', getPostComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
 
 export default router;
